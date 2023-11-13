5e85fb5bfcbedd5996ee2775eca915102ca3f0c7
Muhammad Alam <malam53345@gmail.com>
2023-11-13 10:55:49 +0500

    chat app UI completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <ChakraProvider>
     <BrowserRouter>
-      <App />
+      <ChatProvider>
+        <App />
+      </ChatProvider>
     </BrowserRouter>
   </ChakraProvider>,
   document.getElementById("root")
