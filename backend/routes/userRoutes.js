728dedbdb6469a4c4d651b120cf54404115a3949
Muhammad Alam <malam53345@gmail.com>
2023-11-17 15:49:27 +0500

    remove build folder

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  userRegister,
+  authUser,
+  allUsers,
+} from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", protect, allUsers);
+router.post("/", userRegister);
+router.post("/login", authUser);
+
+export default router;
