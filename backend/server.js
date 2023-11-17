62eff4c479aa9822ccaea36caa97111782245c4b
Muhammad Alam <malam53345@gmail.com>
2023-11-17 16:38:28 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
     origin: "https://chat-app-sandy-alpha.vercel.app",
-    // credentials: true,
+    credentials: true,
   },
 });
 
