ebf6a479f3afc80872a6e8375c2e03a6481a1c70
Muhammad Alam <malam53345@gmail.com>
2023-11-17 15:15:57 +0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import chatRoutes from "./routes/chatRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from "path";
 
 dotenv.config();
 connectDB();
@@ -15,14 +16,29 @@ connectDB();
 const app = express();
 app.use(express.json()); // to accept json data
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
